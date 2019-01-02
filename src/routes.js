17c7a137f53d56f71a3c4a5886786b65afb6eef6
Petar Vorotnikov <petar@vorotnikov.net>
2019-01-02 15:05:46 +0200

    Created sample implementation for event callback

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,9 @@
 const express = require('express')
+const axios = require('axios')
+const nconf = require('nconf')
+const { logger, } = require('./lib')
+
+const SLACK_URL = 'https://slack.com/api/chat.postMessage'
 
 module.exports = function(app) {
 
@@ -8,5 +13,91 @@ module.exports = function(app) {
     router.get('/', async (req, res, next) => {
         res.json({ status: 'ok'})
     })
+
+    router.post('/', async (req, res, next) => {
+        try {
+
+            switch (req.body.type) {
+
+                // verification URL is used to ensure the endpoint is valid
+                // and trusted by Slack
+                case 'url_verification':
+                    res.send(req.body.challenge)
+                    // TODO: save the token
+                    break
+
+                // typical event callback
+                // always respond with 200 in order to
+                // please Slack with "swift response"
+                case 'event_callback':
+                    // TODO: validate the token
+                    await handleEvent(req)
+                    res.json({ status: 'ok' })
+                    break
+
+                // well, we are rate limited.
+                // c'est la vie :(
+                case 'app_rate_limited':
+                    // TODO: validate the token
+                    res.json({ status: 'ok' })
+                    break
+
+                // by default respond with 400
+                // for invalid event types
+                default:
+                    res.status(400).json({
+                        status: 'error',
+                        errorCode: 'INVALID_EVENT_TYPE',
+                        errorMessage: 'Invalid event type'
+                    })
+            }
+
+        } catch (err) {
+            res.status(500).json({
+                status: 'error',
+                errorCode: 'GENERAL_ERROR',
+                errorMessage: err.message
+            })
+        }
+
+    })
+
+    async function handleEvent(req) {
+        try {
+            logger.info(JSON.stringify(req.body))
+
+            if ('message' !== req.body.event.type) {
+                throw new Error(`I don't support this event type: ${req.body.event.type}`)
+            }
+
+            if ('bot_message' === req.body.event.subtype) {
+                 throw new Error(`We don't serve your kind here!`)
+            }
+
+            // user - the ID of the user who sent the message
+            // channel - the ID of the channel where the message was posted
+            // text - the text of the message
+            const { user, text, channel } = req.body.event
+
+            // TODO: handle the event here based on text
+            logger.info('User ID: ' + user)
+            logger.info('Channel ID: ' + channel)
+            logger.info('Text: ' + text)
+
+            // Ignore messages not starting with 'bot'
+            if (!text.startsWith('bot')) {
+                throw new Error('This does not concern me')
+            }
+
+            let response = await axios.post(SLACK_URL + `?channel=${channel}&token=${nconf.get('BOT_USER_TOKEN')}`, {
+                text: `The answer to your query (${text}) is 42`,
+            })
+            logger.info(response.data)
+
+        } catch (err) {
+            logger.warn(err.message)
+        }
+
+    }
 
 }
--- src/tests/unit/routes.spec.js
+++ src/tests/unit/routes.spec.js
@@ -23,4 +23,83 @@ describe('Tests routes', function () {
         res.status.should.equal(200)
     })
 
+    it('POST / url_verification', async () => {
+
+        const res = await request(app)
+        .post('/')
+        .send({
+            token: 'some-token',
+            challenge: 'some-challenge',
+            type: 'url_verification'
+        })
+
+        res.status.should.equal(200)
+        res.text.should.equal('some-challenge')
+
+    })
+
+    it('POST / unknown', async () => {
+
+        const res = await request(app)
+        .post('/')
+        .send({ type: 'unknown' })
+
+        res.status.should.equal(400)
+        res.body.status.should.equal('error')
+
+    })
+
+    it('POST / event_callback', async () => {
+
+        moxios.stubRequest(/\/api\/chat.postMessage/, {
+            status: 200,
+            response: {
+                ok: true,
+                channel: 'C1H9RESGL',
+                ts: '1503435956.000247',
+                message: {
+                    text: "Here's a message for you",
+                    username: 'ecto1',
+                    bot_id: 'B19LU7CSY',
+                    attachments: [{
+                        text: 'This is an attachment',
+                        id: 1,
+                        fallback: "This is an attachment's fallback"
+                    }],
+                    type: 'message',
+                    subtype: 'bot_message',
+                    ts: '1503435956.000247'
+                }
+            }
+        })
+
+        const res = await request(app)
+        .post('/')
+        .send({
+            token: 'same-token',
+            team_id: '123456789',
+            api_app_id: 'ABCDEFGHI',
+            event: {
+                client_msg_id: '5e33bc80-0839-421e-9ff9-3426a7beac63',
+                type: 'message',
+                text: 'bot, go ahead',
+                user: 'A1B2C3D4E',
+                ts: '1546432717.003800',
+                channel: 'ZXYGKKGVK',
+                event_ts: '1546432717.003800',
+                channel_type: 'channel'
+            },
+            type: 'event_callback',
+            event_id: 'EvF510L537',
+            event_time: 1546432717,
+            authed_users: [
+                'A1B2C3D4E'
+            ]
+        })
+
+        res.status.should.equal(200)
+        res.body.status.should.equal('ok')
+
+    })
+
 })
