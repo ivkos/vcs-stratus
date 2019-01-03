d4bea429983ebea26625c5275cf301dc1fc8922e
Ivaylo Stoyanov <istoyan1@visteon.com>
2019-01-03 18:41:24 +0200

    Add initial Dialogflow integration

--- src/event.js
+++ src/event.js
@@ -5,6 +5,10 @@ const axios = require('axios')
 const { logger, } = require('./lib')
 const SLACK_URL = 'https://slack.com/api/chat.postMessage'
 
+const dialogflow = require('dialogflow')
+const uuid = require('uuid')
+const path = require('path')
+
 async function handleEvent(e) {
 
     try {
@@ -35,17 +39,46 @@ async function handleEvent(e) {
             throw new Error('This does not concern me')
         }
 
-        // return message to Slack
-        const responseText = `The answer to your query (${text}) is 42`
-        const uri = SLACK_URL + `?channel=${channel}&token=${nconf.get('BOT_USER_TOKEN')}&text=${encodeURIComponent(responseText)}`
-        const response = await axios.post(uri)
+        const sessionClient = new dialogflow.SessionsClient({
+            keyFilename: path.join(__dirname, 'secrets', nconf.get('GOOGLE_APPLICATION_CREDENTIALS_FILENAME'))
+	})
+        const sessionPath = sessionClient.sessionPath(nconf.get('DIALOGFLOW_PROJECT_ID'), uuid.v4())
+        const dialogflowRequest = {
+            session: sessionPath,
+            queryInput: {
+                text: { text: text, languageCode: 'en-US' },
+            },
+        }
 
-        logger.info('Slack response: ' + JSON.stringify(response.data))
+        const responses = await sessionClient.detectIntent(dialogflowRequest)
+        const result = responses[0].queryResult
+        const fulfillmentText = result.fulfillmentText
+
+        if (result.intent) {
+            logger.info(`Matched intent: ${result.intent.displayName}`)
+
+            if (fulfillmentText) {
+                await sendMessage(channel, fulfillmentText)
+            }
+        } else {
+            logger.warn("Did not match any intent")	
+            if (fulfillmentText) {
+                await sendMessage(channel, fulfillmentText)
+            } else {
+                await sendMessage(channel, "I don't know what you mean")
+	    }
+        }
 
     } catch (err) {
         logger.warn(err.message)
     }
 
+}
+
+async function sendMessage(channel, message) {
+    const uri = SLACK_URL + `?channel=${channel}&token=${nconf.get('BOT_USER_TOKEN')}&text=${encodeURIComponent(message)}`
+    const response = await axios.post(uri)
+    logger.info('Slack response: ' + JSON.stringify(response.data))
 }
 
 exports.handler = async function (event, context) {
